2e394d66b2065aed4c25743309010cdf17fbfc9e
Kristian Simov <krisi.simov@gmail.com>
2022-01-01 01:41:00 +0200

    New tests added to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,37 @@
 #include "DRandomForest.h"
 
+#include <chrono>
+
 const unsigned int MAX_SAMPLES = 10000;
 
 int main()
 {
     
     DData trainingData("fruit training set.csv", MAX_SAMPLES);
+    DData testData("test set.csv", MAX_SAMPLES);
 
     DRandomForest randomForest;
-
     randomForest.fit(trainingData);
 
-    DValue prediction = randomForest.classify(DSample(12, { DValue(47.1345), DValue(1.0), DValue(7653.831), DValue(1.0), DValue("?") }), trainingData);
-    std::cout << prediction;
+    auto now = std::chrono::system_clock::now();
+    auto UTC = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
 
-    /*
-    DTree dtree;
 
-    dtree.fit(trainingData);
+    std::ofstream results("test results " + std::to_string(UTC) + ".txt");
 
-    prediction = dtree.classify(DSample(12, { DValue(47.1345), DValue(1.0), DValue(7653.831), DValue(1.0), DValue("?") }), trainingData);
-    std::cout << prediction;
-    */
-    
+    for (auto sample : testData.getSamples())
+    {
+        
+
+        DValue prediction = randomForest.classify(sample, trainingData);
+
+        results << "prediction: " << prediction.getNumericValue() 
+                << " actual: " << sample.getTargetClassNumericValue() 
+                << " out-of-bag-error" << randomForest.getStrength() 
+                << "\n";
+    }
+
+    results.close();
 
     return 0;
 
