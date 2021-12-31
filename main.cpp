5167427cb6850374a1a30c6aafb4ace75bb9c0be
Kristian Simov <krisi.simov@gmail.com>
2021-12-31 06:06:20 +0200

    Added test for random forest in main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,22 @@ int main()
     
     DData trainingData("fruit training set.csv", MAX_SAMPLES);
 
+    DRandomForest randomForest;
+
+    randomForest.fit(trainingData);
+
+    DValue prediction = randomForest.classify(DSample(12, { DValue(47.1345), DValue(1.0), DValue(7653.831), DValue(1.0), DValue("?") }), trainingData);
+    std::cout << prediction;
+
+    /*
     DTree dtree;
 
     dtree.fit(trainingData);
 
-    DValue prediction = dtree.classify(DSample(12, { DValue(47.1345), DValue(1.0), DValue(7653.831), DValue(1.0), DValue("?") }), trainingData);
+    prediction = dtree.classify(DSample(12, { DValue(47.1345), DValue(1.0), DValue(7653.831), DValue(1.0), DValue("?") }), trainingData);
     std::cout << prediction;
+    */
+    
 
     return 0;
 
