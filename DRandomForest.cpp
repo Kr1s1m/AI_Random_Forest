fc256419d3d9dfc278d16084426d14d555b4907d
Kristian Simov <krisi.simov@gmail.com>
2022-01-07 00:44:43 +0200

    DRandomForest - now suports multithreading functionality. Constructor adjusted. Console messages updated.

--- DRandomForest.cpp
+++ DRandomForest.cpp
@@ -1,19 +1,20 @@
 #include "DRandomForest.h"
 
 
-DRandomForest::DRandomForest(unsigned int _dtreeCount, unsigned int _maxDepth, unsigned int _minSamplesPerSplit,
-	unsigned int _minSamplesPerLeaf, double _impurityThreshold, bool _bootstrappingAllowed, bool _regression,
-	ImpurityFunctor _impurityFunction, FeatureFunctor _featureFunction):
+DRandomForest::DRandomForest(unsigned int _dtreeCount, unsigned int _maxDepth, unsigned int _minSamplesPerSplit, 
+							 unsigned int _minSamplesPerLeaf, double _impurityThreshold,
+							 bool _bootstrappingAllowed, bool _regression, bool _multithread,
+							 ImpurityFunctor _impurityFunction, FeatureFunctor _featureFunction) :
 
+	threadCount(std::thread::hardware_concurrency()),
 	dtreeCount(_dtreeCount), maxDepth(_maxDepth), minSamplesPerSplit(_minSamplesPerSplit),
 	minSamplesPerLeaf(_minSamplesPerLeaf), impurityThreshold(_impurityThreshold),
 	bootstrappingAllowed(_bootstrappingAllowed), regression(_regression),
-	isTrained(false), outOfBagError(-1.0),
+	multithread(_multithread), isTrained(false), outOfBagError(100.0),
 	impurityFunction(_impurityFunction), featureFunction(_featureFunction)
+
 {
-	decisionTrees = std::vector<DTree>(dtreeCount,
-		DTree(maxDepth, minSamplesPerSplit, minSamplesPerLeaf, impurityThreshold,
-			  bootstrappingAllowed, regression, impurityFunction, featureFunction));
+	decisionTrees.reserve(_dtreeCount);
 }
 
 
@@ -32,37 +33,94 @@ void DRandomForest::calculateOutOfBagError()
 	outOfBagError = errorSum / (double)dtreeCount;
 }
 
+
+void DRandomForest::multiThreadFit(const DData& data)
+{
+	ThreadPool threadPool(threadCount);
+	std::vector<std::future<DTree>> poolResults;
+
+	for (unsigned int treeIndex = 0; treeIndex < dtreeCount; treeIndex++)
+	{
+		poolResults.emplace_back(threadPool.enqueue(
+
+			[&, treeIndex]()
+			{
+
+				DTree dtree(maxDepth, minSamplesPerSplit, minSamplesPerLeaf, impurityThreshold,
+							bootstrappingAllowed, regression,
+							impurityFunction, featureFunction
+							);
+
+				dtree.fit(data);
+
+				return dtree;
+
+			}
+
+		));
+	}
+
+	std::vector<std::future<DTree>>::iterator resultIterator = poolResults.begin();
+
+	for (resultIterator; resultIterator != poolResults.end(); resultIterator++)
+		decisionTrees.push_back(resultIterator->get());
+
+}
+
+void DRandomForest::mainThreadFit(const DData& data)
+{
+
+	for (unsigned int treeIndex = 0; treeIndex != dtreeCount; treeIndex++)
+	{
+		DTree dtree(maxDepth, minSamplesPerSplit, minSamplesPerLeaf, impurityThreshold,
+					bootstrappingAllowed, regression,
+					impurityFunction, featureFunction
+				   );
+
+		dtree.fit(data);
+
+		decisionTrees.push_back(dtree);
+	}
+}
+
 void DRandomForest::fit(const DData& data)
 {
-	unsigned int treeIndex = 0;
-	unsigned int remainingTrees = dtreeCount;
 
-	std::vector<DTree>::iterator treeIt = decisionTrees.begin();
+	std::cout << "Fitting trees to \"" << data.getFileName() << "\", please wait.... ";
 
-	for (treeIt; treeIt != decisionTrees.end(); treeIt++)
+	if (multithread)
 	{
-		system("cls");
-
-		std::cout << "Fitting tree "         << treeIndex++      
-				  << "\nTrees in fit queue: " << --remainingTrees
-				  << "\n";
-
-		treeIt->fit(data);
+		multiThreadFit(data);
+	}	
+	else
+	{
+		mainThreadFit(data);
 	}
-		
-	system("cls");
-
-	std::cout << "Fitting \"" << data.getFileName() << "\" has finished.\n";
-		      
-
+			
 	isTrained = true;
 
-	std::cout << "Calculating out-of-bag error for forest...\n";
+	std::cout << "Done.\n";
+		   
+	std::cout << "Calculating out-of-bag error for forest... ";
 
 	calculateOutOfBagError();
 
 	std::cout << "Done.\n"
-	<< "Your random forest is now trained and ready for use!\n\n";
+	<< "Your random forest is now trained and ready.\n\n";
+}
+
+void DRandomForest::reset()
+{
+	if (!isTrained)
+		return;
+
+	std::cout << "Resetting forest... ";
+
+	decisionTrees.clear();
+
+	isTrained = false;
+
+	std::cout << "Done.\n\n";
 }
 
 DValue DRandomForest::classify(const DSample& sample) const
@@ -94,14 +152,14 @@ DValue DRandomForest::classify(const DSample& sample) const
 	return DValue(mostVotedClass);
 
 	//return std::max_element(votes.begin(), votes.end(), [](const auto& x, const auto& y) {return x.second < y.second; })->first;
-	
-	
 }
 
 bool DRandomForest::classifyBatch(const DData& testData) const
 {
 	if (!isTrained)
 		return false;
+
+	std::cout << "Batch \"" << testData.getFileName() << "\" classification in progress... ";
 
 	std::string resultsFileName = "results " + std::to_string(time(0)) + ".txt";
 	std::ofstream resultsFile(resultsFileName);
@@ -118,8 +176,8 @@ bool DRandomForest::classifyBatch(const DData& testData) const
 
 	resultsFile.close();
 
-	std::cout << "Batch \""                << testData.getFileName() << "\" classification complete!\n"
-			  << "Results saved inside \"" << resultsFileName        << "\".\n";
+	std::cout << "Done.\n"
+			  << "Results saved inside \"" << resultsFileName << "\".\n\n";
 
 	return true;
 
--- DRandomForest.h
+++ DRandomForest.h
@@ -2,12 +2,15 @@
 
 #include "DTree.h"
 #include <ctime>
+#include "ThreadPool.h"
 
 class DRandomForest
 {
 private:
 
 	std::vector<DTree> decisionTrees;
+
+	unsigned int threadCount;
 
 	unsigned int dtreeCount;
 	unsigned int maxDepth;
@@ -20,18 +23,23 @@ private:
 	bool bootstrappingAllowed;
 	bool regression;
 	bool isTrained;
+	bool multithread;
 
 	ImpurityFunctor impurityFunction;
 	FeatureFunctor featureFunction;
+
+	void multiThreadFit(const DData&);
+	void mainThreadFit(const DData&);
 
 	void calculateOutOfBagError();
 
 public:
 
-	DRandomForest(unsigned int = 200, unsigned int = 10, unsigned int = 1, unsigned int = 1, double = 0.01,
-		bool = true, bool = false, ImpurityFunctor = calculateShannonEntropy, FeatureFunctor = squareRoot);
+	DRandomForest(unsigned int = 200, unsigned int = 7, unsigned int = 1, unsigned int = 1, double = 0.01,
+		bool = true, bool = false, bool = true, ImpurityFunctor = calculateShannonEntropy, FeatureFunctor = squareRoot);
 
 	void fit(const DData&);
+	void reset();
 
 	DValue classify(const DSample&)const;
 
